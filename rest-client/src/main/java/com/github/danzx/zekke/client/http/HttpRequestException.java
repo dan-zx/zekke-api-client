4210b1c37dd7c98fa7f7244bd3367f31306478b6
Dan <danyboy_shaggy@hotmail.com>
2017-09-27 20:42:36 -0500

    Remove usless methods

--- rest-client/src/main/java/com/github/danzx/zekke/client/http/HttpRequestException.java
+++ rest-client/src/main/java/com/github/danzx/zekke/client/http/HttpRequestException.java
@@ -11,8 +11,4 @@ public class HttpRequestException extends RuntimeException {
     public HttpRequestException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public HttpRequestException(Throwable cause) {
-        super(cause);
-    }
 }
--- rest-client/src/main/java/com/github/danzx/zekke/client/http/HttpResponseBodyException.java
+++ rest-client/src/main/java/com/github/danzx/zekke/client/http/HttpResponseBodyException.java
@@ -11,8 +11,4 @@ public class HttpResponseBodyException extends RuntimeException {
     public HttpResponseBodyException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public HttpResponseBodyException(Throwable cause) {
-        super(cause);
-    }
 }
